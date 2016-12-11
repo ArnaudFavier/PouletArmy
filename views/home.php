11941ee99966ff282f6d6543f2232348427b1db3
Arnaud <arnaudfavier@users.noreply.github.com>
2016-12-11 18:44:53 +0100

    Removed new weather message

--- views/home.php
+++ views/home.php
@@ -38,8 +38,6 @@
 	</form>
 </div>
 
-<div class="alert alert-warning" role="alert">Nouveauté : la météo lors des attaques !</div>
-
 <div id="home-description" class="panel panel-default">
 	<div class="panel-body">
 		<p><strong>Poulet Army</strong> est un jeu stratégique de combats de poulets.</p>
