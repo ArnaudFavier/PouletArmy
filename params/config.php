56f0becfd97a08e1ebd0d06fdae8ad6b19ece12f
Arnaud <arnaudfavier@users.noreply.github.com>
2017-01-27 17:35:47 +0100

    Add default config file

---
+++ params/config.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ |------------------------------
+ | 		Classe Config
+ | Configuration de l'environnement.
+ |------------------------------
+ */
+
+class Config {
+
+	const DB_HOST = 'localhost';
+	const DB_NAME = 'pouletarmy';
+	const DB_LOGIN = 'root';
+	const DB_PASSWORD = '';
+	const DB_TABLE_PREFIX = 'pa_';
+
+	const PASSWORD_SALT = 'Arnaud';
+
+}
